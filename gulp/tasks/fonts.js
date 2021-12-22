fad4cda2703a5322c68c012d8512c75389bc821c
keydies <serkeydies@gmail.com>
2021-12-22 17:58:02 +0200

    gulp-build-ready

---
+++ gulp/tasks/fonts.js
@@ -0,0 +1,56 @@
+import fonter from 'gulp-fonter';
+import ttf2woff2 from 'gulp-ttf2woff2';
+
+export const otfToTtf = () => {
+	// Ищем файлы шрифтов .otf
+	return (
+		app.gulp
+			.src(`${app.path.scrFolder}/fonts/*.otf`, {})
+			.pipe(
+				app.plugins.plumber(
+					app.plugins.notify.onError({
+						title: 'FONTS',
+						message: 'Error: <%= error.message %>',
+					})
+				)
+			)
+			// Конвертируем в .ttf
+			.pipe(
+				fonter({
+					formats: ['ttf'],
+				})
+			)
+			// Выгружаем в исходную папку
+			.pipe(app.gulp.dest(`${app.path.scrFolder}/fonts/`))
+	);
+};
+
+export const ttfToWoff = () => {
+	// Ищем файлы шрифтов .ttf
+	return (
+		app.gulp
+			.src(`${app.path.scrFolder}/fonts/*.ttf`, {})
+			.pipe(
+				app.plugins.plumber(
+					app.plugins.notify.onError({
+						title: 'FONTS',
+						message: 'Error: <%= error.message %>',
+					})
+				)
+			)
+			// Конвертируем в .woff
+			.pipe(
+				fonter({
+					formats: ['woff'],
+				})
+			)
+			// Выгружаем в папку с результатом
+			.pipe(app.gulp.dest(`${app.path.build.fonts}`))
+			// Ищем файлы шрифтов .ttf
+			.pipe(app.gulp.src(`${app.path.scrFolder}/fonts/*.ttf`))
+			// Конвертируем в woff2
+			.pipe(ttf2woff2())
+			// Выгружаем в папку с результатом
+			.pipe(app.gulp.dest(`${app.path.build.fonst}`))
+	);
+};
---
+++ src/js/app.js
@@ -0,0 +1,3 @@
+import * as keydiesFunc from './modules/functions.js';
+
+keydiesFunc.isWebp();
